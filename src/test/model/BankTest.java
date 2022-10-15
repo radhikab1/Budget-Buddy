800ef48bf076b789e4696a0eb9a82c6877add024
Radhika Bajaj <rbajaj@students.cs.ubc.ca>
2022-10-15 11:21:19 -0700

    Tested and Implemented Account and Bank with working tests/implementation

--- src/test/model/BankTest.java
+++ src/test/model/BankTest.java
@@ -4,63 +4,62 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class BankTest {
-    private List<Account> accounts;
+    private Bank bank;
     private Account account1;
     private Account account2;
 
     @BeforeEach
     public void runBefore() {
-        accounts = new ArrayList<>();
+        bank = new Bank();
         account1 = new Account("John Smith", 200.00);
         account2 = new Account("Emily Wood", 0.00);
     }
 
     @Test
     public void testConstructor() {
-        assertNotNull(accounts.getAccounts());
-        assertEquals(0, accounts.getAccounts().size());
+        assertNotNull(bank.getAccounts());
+        assertEquals(0, bank.getAccounts().size());
     }
 
     @Test
     public void testAddAccountOnce() {
-        accounts.addAccount(account1);
-        assertEquals(1, accounts.size());
-        assertEquals(account1, accounts.get(1));
+        bank.addAccount(account1);
+        assertEquals(1, bank.getAccounts().size());
+        assertEquals(account1, bank.getAccounts().get(0));
     }
 
     @Test
     public void testAddAccountMultipleTimes() {
-        accounts.addAccount(account1);
-        accounts.addAccount(account2);
-        assertEquals(2, accounts.size());
-        assertEquals(account1, accounts.get(1));
-        assertEquals(account2, accounts.get(2));
+        bank.addAccount(account1);
+        bank.addAccount(account2);
+        assertEquals(2, bank.getAccounts().size());
+        assertEquals(account1, bank.getAccounts().get(0));
+        assertEquals(account2, bank.getAccounts().get(1));
     }
 
     @Test
     public void testRemoveAccountOnce() {
-        accounts.add(account1);
-        accounts.add(account2);
-        accounts.removeAccount(account2);
-        assertEquals(1, accounts.size());
-        assertEquals(account1, accounts.get(1));
+        bank.getAccounts().add(account1);
+        bank.getAccounts().add(account2);
+        bank.removeAccount(account2);
+        assertEquals(1, bank.getAccounts().size());
+        assertEquals(account1, bank.getAccounts().get(0));
     }
 
     @Test
     public void testRemoveAccountMultipleTimes(){
-        accounts.add(account1);
-        accounts.add(account2);
-        accounts.removeAccount(account2);
-        assertEquals(1, accounts.size());
-        assertEquals(account1, accounts.get(1));
-        accounts.removeAccount(account1);
-        assertEquals(0, accounts.size());
+        bank.getAccounts().add(account1);
+        bank.getAccounts().add(account2);
+        bank.removeAccount(account2);
+        assertEquals(1, bank.getAccounts().size());
+        assertEquals(account1, bank.getAccounts().get(0));
+        bank.removeAccount(account1);
+        assertEquals(0, bank.getAccounts().size());
     }
 }
 
