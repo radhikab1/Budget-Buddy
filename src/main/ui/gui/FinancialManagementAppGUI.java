df3e51f23c0d7be8df8eea791e11681b1c5b611f
Radhika Bajaj <rbajaj@students.cs.ubc.ca>
2022-11-13 20:53:00 -0800

    Added citations for PHase 3 to FinancialManagementAppGUI

--- src/main/ui/gui/FinancialManagementAppGUI.java
+++ src/main/ui/gui/FinancialManagementAppGUI.java
@@ -25,6 +25,28 @@ import java.io.IOException;
 // Used for JsonReader and JsonWriter and their tests, Writable, Main,
 // and loadAccountsList() and saveAccountsList() in FinancialManagementApp
 //
+// https://www.javatpoint.com/java-jframe
+// Used to determine functionality of JFrame
+//
+// https://www.javatpoint.com/java-jpanel, https://www.codejava.net/java-se/swing/jpanel-basic-tutorial-and-examples
+// Used to determine functionality of JPanel
+//
+// https://github.students.cs.ubc.ca/CPSC210/AlarmSystem
+// Used to determine how to perform event handling and add buttons, and how to display pop-up boxes
+// (question messages, information messages, error messages)
+//
+// https://stackoverflow.com/questions/19506769/how-to-center-jlabel-in-jframe-swing
+// Used to determine how to center JLabel
+//
+// https://www.tutorialsfield.com/java-splash-screen-with-progress-bar/
+// Used for creating SplashScreen with image, progressbar, and loading message
+//
+// https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngkey.com%2Fdetail%2Fu2e6r5q8e6y3o0y3_free-shipping-on-a-per-product-basis-in%2F&psig=AOvVaw3J5rtwrDi_HiQSnUnAx9bM&ust=1668276375258000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLCS3aLurPsCFQAAAAAdAAAAABAD
+// Splash screen image
+//
+// https://stackoverflow.com/questions/16343098/resize-a-picture-to-fit-a-jlabel
+// Used to determine how to resize image to fit JLabel
+//
 // Represents main class. Executes main method and instantiates FinancialManagementApp
 public class FinancialManagementAppGUI extends JFrame {
     private static final int FRAME_WIDTH = 600;
